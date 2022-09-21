541f3a94f0f279452496733adc06a283e47e8c18
Kate <kit.ty.kate@disroot.org>
2022-09-21 23:19:07 +0100

    Remove the hand-written META file and simply takes what dune generates. This forces people who use ocamlfind to choose between shared and static at link time

--- install.sh
+++ install.sh
@@ -9,19 +9,5 @@ if test "$#" != 1; then
 fi
 
 PREFIX=$1
-VERSION=$(cat VERSION)
 
 dune install "--prefix=$PREFIX" --release
-
-for pkg in $(basename -s .opam *.opam); do 
-    case "$pkg" in
-    llvm)
-        sed -e "s/@VERSION@/$VERSION/g" META.llvm.in > "$PREFIX/lib/llvm/META";;
-    llvm_*)
-        target=$(echo "$pkg" | cut -d_ -f2-)
-        sed -e "s/@VERSION@/$VERSION/g" -e "s/@TARGET@/$target/g" META.llvm_TARGET.in > "$PREFIX/lib/llvm_$target/META";;
-    *)
-        echo "Something went wrong while processing $pkg. Please report."
-        exit 1;;
-    esac
-done
